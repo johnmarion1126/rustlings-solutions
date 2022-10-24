1525d5a395abc7c352d7ffe44087680794e45579
John Sanchez <johnmarion1126@gmail.com>
2022-10-24 20:08:54 +0000

    Add another method to errors2.rs

--- errors/errors2.rs
+++ errors/errors2.rs
@@ -26,6 +26,12 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>()?;
 
+    // Other method
+    // let qty = match item_quantity.parse::<i32>() {
+    //     Ok(x) => x,
+    //     Err(e) => return Err(e),
+    // };
+
     Ok(qty * cost_per_item + processing_fee)
 }
 
